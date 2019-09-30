0f7a5deba55246b11ce97ab875dc4a6a4f677465
immais <mais@intermedia.org>
2019-09-30 09:45:11 -0400

    Update Merge data_sy.R

--- SPSS Syntax by section/Merge data_sy.R
+++ SPSS Syntax by section/Merge data_sy.R
@@ -7,53 +7,53 @@ library(readxl)
 ##Input data
 
 {#Bangladesh
-  b13<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  b14<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  b15<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  b16<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  b17<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  b18<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2018 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  b13<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  b14<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  b15<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  b16<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  b17<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  b18<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Bangladesh 2013-2018/FII Bangladesh 2018 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
  #India
-  i13<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  i14<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  i15<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  i16<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  i17<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  i18<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2018 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  i13<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  i14<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  i15<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  i16<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  i17<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  i18<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/India 2013-2018/FII India 2018 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
   #Indonesia
-  ido14<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indonesia 2014-2016/FII Indonesia 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  ido15<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indonesia 2014-2016/FII Indonesia 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  ido16<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indonesia 2014-2016/FII Indonesia 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  ido14<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indonesia 2014-2016/FII Indonesia 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  ido15<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indonesia 2014-2016/FII Indonesia 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  ido16<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indonesia 2014-2016/FII Indonesia 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
   #Kenya
-  k13<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  k14<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  k15<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  k16<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  k17<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  k13<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  k14<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  k15<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  k16<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  k17<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Kenya 2013-2017/FII Kenya 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
   #Nigeria
-  n13<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2013 (public+sbjnum).sav",to.data.frame=T,use.value.labels = T)
-  n14<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  n15<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  n16<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  n17<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  n13<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2013 (public+sbjnum).sav",to.data.frame=T,use.value.labels = T)
+  n14<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  n15<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  n16<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  n17<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Nigeria 2013-2017/FII Nigeria 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
   #Pakistan
-  p13<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  p14<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  p15<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  p16<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  p17<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  p13<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  p14<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  p15<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  p16<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  p17<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Pakistan 2013-2017/FII Pakistan 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
   #Tanzania
-  t13<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2013 (public+sbjnum).sav",to.data.frame=T,use.value.labels = T)
-  t14<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  t15<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  t16<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  t17<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  t13<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2013 (public+sbjnum).sav",to.data.frame=T,use.value.labels = T)
+  t14<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  t15<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  t16<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  t17<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Tanzania 2013-2017/FII Tanzania 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
   #Uganda
-  u13<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  u14<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  u15<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  u16<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
-  u17<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  u13<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2013 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  u14<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2014 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  u15<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2015 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  u16<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2016 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+  u17<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Uganda 2013-2017/FII Uganda 2017 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
 }
 
 ##Merge data
@@ -66,7 +66,7 @@ merge.FII.da <- function(sec_num){
                "b14","i14","ido14","k14","n14","p14","t14","u14",
                "b13", "i13","k13","n13","p13","t13","u13")
   key <- c("SBJNUM","COUNTRY","YEAR")
-  var_list <- read_excel("C:/Users/tangh/Documents/bb/Codebook/Variable list_2019.xlsx")
+  var_list <- read_excel("C:/Users/maish.KTPDCLHB1JQV2/Documents/GitHub/Codebook/Variable list_2019.xlsx")
   var_list <- as.data.frame(var_list)[which(var_list$Section==sec_num),c(2,3)]
   sec <- as.character(var_list$`Variable name in Datathon`)
   sec <- c(key,sec)
@@ -74,8 +74,8 @@ merge.FII.da <- function(sec_num){
   for(i in 1:length(da_name)){
     m_sec <- rbind(m_sec,select(eval(parse(text=da_name[i])),sec))
   }
-  write.csv(m_sec,paste0("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Section ",sec_num,".csv")) 
-  write_sav(m_sec,paste0("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Section ",sec_num,".sav"))
+  write.csv(m_sec,paste0("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Section ",sec_num,".csv")) 
+  write_sav(m_sec,paste0("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Section ",sec_num,".sav"))
 }
 
 ##Section 3
@@ -154,14 +154,14 @@ merge.FII.da(8.1)
 merge.FII.da(1)
 
 ##Indonesia 2018
-var_list <- read_excel("C:/Users/tangh/Documents/bb/Codebook/Variable list_2019.xlsx")
+var_list <- read_excel("C:/Users/maish.KTPDCLHB1JQV2/Documents/GitHub/Codebook/Variable list_2019.xlsx")
 var_list <- as.character(as.data.frame(var_list)[,3])
 
-ido18<-read.spss("C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indonesia 2014-2016/FII Indonesia 2018 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
+ido18<-read.spss("C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indonesia 2014-2016/FII Indonesia 2018 (public+ANONGPS).sav",to.data.frame=T,use.value.labels = T)
 ido18 <- select(ido18,var_list)
          
-write.csv(ido18,"C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indo_2018.csv")
-write_sav(ido18,"C:/Users/tangh/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indo_2018.sav")
+write.csv(ido18,"C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indo_2018.csv")
+write_sav(ido18,"C:/Users/maish.KTPDCLHB1JQV2/Intermedia Survey Institute/DA Team - General/Shiyun/2019/Datathon/Data/Indo_2018.sav")
 
 ###master data
 {
